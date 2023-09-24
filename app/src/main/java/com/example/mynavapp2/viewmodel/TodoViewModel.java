43b127f5edf5c481ef2aa9992393f3801de04d6a
Don-Carlos <syldeasdoncarlos@gmail.com>
2023-09-24 04:30:05 +0200

    first commit

--- app/src/main/java/com/example/mynavapp2/viewmodel/TodoViewModel.java
+++ app/src/main/java/com/example/mynavapp2/viewmodel/TodoViewModel.java
@@ -17,16 +17,21 @@ public class TodoViewModel extends ViewModel {
     public MutableLiveData<Boolean> loading = new MutableLiveData<Boolean>();
 
     public void refresh() {
-        Todo todo1 = new Todo("First Todo1", false);
-        Todo todo2 = new Todo("First Todo2", false);
-        Todo todo3 = new Todo("First Todo3", false);
-        Todo todo4 = new Todo("First Todo4", false);
+        Todo todo1 = new Todo("First Todo 1", false);
+        Todo todo2 = new Todo("First Todo 2", false);
+        Todo todo3 = new Todo("First Todo 3", false);
+        Todo todo4 = new Todo("First Todo 4", false);
+        Todo todo5 = new Todo("First Todo 5", false);
+        Todo todo6 = new Todo("First Todo 6", false);
+
         List<Todo> todoList = new ArrayList<>();
 
         todoList.add(todo1);
         todoList.add(todo2);
         todoList.add(todo3);
-        todoList.add(todo4);
+        todoList.add(todo5);
+        todoList.add(todo6);
+
 
         todos.setValue(todoList);
         loadError.setValue(false);
